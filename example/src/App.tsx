aba611a53e558182fbfd6b6b20b71db22e3fb2b6
Aditya Meilaz Rahmanto <aditya.rahmanto@allianz.co.id>
2021-08-16 14:19:09 +0800

    feat: able to set alpha from background props and update example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -14,22 +14,30 @@ export default function App() {
     <>
       <View style={styles.container}>
         <WheelPickerExpo
+          backgroundColor="#F00F00"
+          selectedStyle={styles.selectedStyle1}
           height={300}
           selectedBorderHeight={2}
           initialSelectedIndex={1}
           items={PROVINCES.map((name) => ({ label: name, value: '' }))}
           onChange={({ item }) => setProvince(item.label)}
           renderItem={(props) => (
-            <Text style={[styles.text, { fontSize: props.fontSize }]}>
+            <Text
+              style={[
+                styles.text,
+                { fontSize: props.fontSize, color: props.fontColor },
+              ]}
+            >
               {props.label}
             </Text>
           )}
         />
 
         <WheelPickerExpo
+          backgroundColor="#202124"
+          selectedStyle={styles.selectedStyle2}
           height={300}
           width={150}
-          selectedBorderColor="red"
           selectedBorderHeight={2}
           initialSelectedIndex={3}
           items={CITIES.map((name) => ({ label: name, value: '' }))}
@@ -65,4 +73,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
+  selectedStyle1: { borderColor: '#202124', borderWidth: 2 },
+  selectedStyle2: { borderColor: '#F00F00', borderWidth: 2 },
 });
---
+++ src/types.ts
@@ -0,0 +1,30 @@
+import type { FlatListProps } from 'react-native';
+
+export type ItemType = { label: string; value: any };
+export type RenderItemProps = {
+  fontSize: number;
+  label: string;
+  fontColor: string;
+};
+
+export interface IViuPickerProps {
+  items: ItemType[];
+  onChange: (item: { index: number; item: ItemType }) => void;
+  initialSelectedIndex?: number;
+  height?: number;
+  width?: any;
+  flatListProps?: Partial<FlatListProps<ItemType>>;
+  backgroundColor?: string;
+  selectedStyle?: {
+    borderColor?: string;
+    borderWidth: number;
+  };
+  renderItem?: (props: RenderItemProps) => JSX.Element;
+}
+
+export interface IViuPickerState {
+  selectedIndex: number;
+  itemHeight: number;
+  listHeight: number;
+  data: ItemType[];
+}
